9c04af45186f55e61899872d426385fe9a2730c1
Adithya U R <adithyaur1999@gmail.com>
2020-09-10 00:12:13 +0200

    Update publish.py

--- publish.py
+++ publish.py
@@ -1,34 +1,44 @@
-#!/usr/bin/env python
 import rospy
 from std_msgs.msg import String, Int8
 
-
-#constants
-topic1 = 'vigneshpurushothamsrinivas'
-topic2 = 'kthfs/result'
+message1 = 'AdithyaUR'
+message2= 'kthfs/result'
 rate = 20
 
 
 def publisher(topic1, topic2, rate=20):
     pub1 = rospy.Publisher(topic1, Int8, queue_size=10)
-    pub2 = rospy.Publisher(topic2, Int8, queue_size=10)
     rospy.init_node('Publisher', anonymous=True)
     rate = rospy.Rate(rate)     # hz
 
     n = 4
+    k=1
     while not rospy.is_shutdown():
-        for k in range(1, 20, n):
+        while(k<20):
+            k=k+4
             rospy.loginfo("publishing %s to topics [%s, %s]" % (k, topic1, topic2))
             pub1.publish(k)
-            pub2.publish(k)
+
+            subscriber(topic=topic)
+
+            pub1.publish(k)
             rate.sleep()
 
+
+
+def callback(data):
+    rospy.loginfo("%s: got message- %s" % (rospy.get_caller_id(), float(data.data)))
+
+
+def subscriber(topic):
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber(topic, Int8, callback)
+    rospy.spin()
+
+
 def main():
-    try:
-        publisher(topic1=topic1, topic2=topic2, rate=rate)
-    except rospy.ROSInterruptException:
-        # print "Program Interrupted"
-        pass
+    publisher(topic1=message1, topic2=message2, rate=rate)
+   
 
 
 if _name== 'main_':
